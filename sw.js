4b74a0f50a4f34af130977ee2876c4c9f1eb1e59
Gour Rishabh <rishabh.gour@evorait.com>
2019-12-31 16:18:02 +0000

    syntax fixes in sw

--- sw.js
+++ sw.js
@@ -20,18 +20,18 @@ self.skipWaiting();
 // 	);
 // });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event){
 
-    var cache = caches.open('resume-cache-v1').then((cache) => {
+    caches.open('northwind-cache-v1').then(function(cache){
 
-        fetch(event.request).then((response) => {
+        fetch(event.request).then(function(response){
             // if (event.request.url.indexOf("sapui5") >= 0) {
                 cache.put(event.request.url, response);
             // }
         });
     });
 
-    event.respondWith(caches.match(event.request).then((response) => {
+    event.respondWith(caches.match(event.request).then(function(response){
         return response || fetch(event.request);
     })
     );
